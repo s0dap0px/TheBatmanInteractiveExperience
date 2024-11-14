2e2b3ba1aa3212332f754e404a40f65fb20c02a9
s0dap0px <160069774+s0dap0px@users.noreply.github.com>
2024-11-14 03:22:02 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// makes the 'code2' image show up!
+function showPicture2() {
+  document.getElementById("code2").src = "images/code2.png";
+}
+
+// makes the 'code3' image show up!
+function showPicture3() {
+  document.getElementById("code3").src = "images/code3.png";
+}
+
+// makes the 'code4' image show up!
+function showPicture4() {
+  document.getElementById("code4").src = "images/code4.png";
+}
+
+// makes the 'code5' image show up!
+function showPicture5() {
+  document.getElementById("code5").src = "images/code5.png";
+}
+
+// makes the 'code6' image show up!
+function showPicture6() {
+  document.getElementById("code6").src = "images/code6.png";
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/z4yybYa90ag";
+}
